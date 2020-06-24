a5f31d9d82d635d5994391f610a940fa85c87c7a
Rajat Jain <1997.rajatjain@gmail.com>
2020-06-24 08:53:20 +0530

    Completed refactoring of rtm_bot

---
+++ mebot/events/message.py
@@ -0,0 +1,25 @@
+from slack import SlackApiError
+from mebot.server import Server
+
+@Server.SLACK_CLIENT.on(event='message')
+async def say_hello(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+    if 'text' in data and 'Hello' in data.get('text', []):
+        channel_id = data['channel']
+        thread_ts = data['ts']
+        user = data['user']
+
+        try:
+            response = web_client.chat_postMessage(
+                channel=channel_id,
+                text=f"Hi <@{user}>!",
+                thread_ts=thread_ts
+            )
+        except SlackApiError as e:
+            # You will get a SlackApiError if "ok" is False
+            assert e.response["ok"] is False
+            # str like 'invalid_auth', 'channel_not_found'
+            assert e.response["error"]
+            print(f"Got an error: {e.response['error']}")
--- mebot/server.py
+++ mebot/server.py
@@ -1,13 +1,36 @@
+import os
+import subprocess
+import yaml
+
 from fastapi import FastAPI
 from mebot.utils.Singleton import Singleton
+from mebot.slack_clients.Client import Client
 # from mebot.options import cli_args
 
 class Server(Singleton):
     APP = None
+    SLACK_CLIENT = None
+    SLACK_TOKEN = None
     def __init__(self):
         super().__init__()
         # debug = True if cli_args.log_level=="DEBUG" else False
         debug=False
         self.__class__.APP = FastAPI(debug=debug)
+        self.__class__.SLACK_CLIENT = Client("RTMClient")
 
-Server()
+app = Server().APP
+
+@app.on_event('startup')
+def get_slack_key():
+    Server.SLACK_TOKEN = yaml.load(subprocess.check_output(
+        ["blackbox_cat", "secrets/keys.yaml.gpg"],
+        stderr=subprocess.DEVNULL,
+        cwd=os.environ["TP_HOME"]), Loader=yaml.FullLoader)["capabiliti_bot"]["bot_token"]
+
+@app.on_event('startup')
+async def boot_slack():
+    Server.SLACK_CLIENT.start(token=Server.SLACK_TOKEN)
+
+@app.on_event('shutdown')
+async def shutdown_slack():
+    await Server.SLACK_CLIENT.stop()
